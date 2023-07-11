e8192e5d18002336f8708aab6d36d1a4843c1275
petergok <petergok@hotmail.com>
2023-07-11 16:43:03 -0400

    Revert "Move .net folder"
    This reverts commit e8b37127386e005becb0563cdc01fcaf75d0e4a7.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  distDir: '../_next',
   reactStrictMode: true,
   images: {
     domains: ["upcdn.io", "replicate.delivery", "lh3.googleusercontent.com"],
