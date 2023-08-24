76809562d5761d5dbdd739a4940189f10dcbc624
paarthmadan <paarth.mdn@gmail.com>
2023-08-24 11:24:55 -0400

    Bump TempoDevtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,9 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 
+import { TempoDevtools } from "@petergok/tempo-devtools";
+TempoDevtools.init();
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
