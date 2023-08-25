47e153a8e4abc89e116e4e9c5dfb9368fabe3dd1
kevin1michael <kevin.michael.905@gmail.com>
2023-08-25 13:34:43 +0000

    Reverted dev tools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,6 @@ import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
-// Import the dev tools and initialize them
-import { TempoDevtools } from "@petergok/tempo-devtools";
-TempoDevtools.init();
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
