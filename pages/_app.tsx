a7cdea8070b686c39cf5361c89c85e888e6fb693
kevin1michael <kevin.michael.905@gmail.com>
2023-09-19 14:50:39 +0000

    added tempo dev tools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,10 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 
+// Import the dev tools and initialize them
+import { TempoDevtools } from "@petergok/tempo-devtools";
+TempoDevtools.init();
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
