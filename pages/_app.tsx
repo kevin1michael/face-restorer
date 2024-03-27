b5a8493aa3e30b6304faebb916abb416c94af1c2
paarthmadan <paarth.mdn@gmail.com>
2024-03-27 19:32:31 -0400

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "../styles/globals.css";
 
+//test
 // Import the dev tools and initialize them
 import { TempoDevtools } from "tempo-devtools";
 TempoDevtools.init();
@@ -14,8 +15,8 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     <SessionProvider session={session}>
       <Component {...pageProps} />
       <Analytics />
-<button onClick={() => setCount(count + 1)}>Click me</button>
-<p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <p>You clicked {count} times</p>
     </SessionProvider>
   );
 }
