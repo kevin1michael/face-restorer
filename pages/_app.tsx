604af6cf9ee6776ca42632d947e0fd45a686d503
petergok <petergok@hotmail.com>
2024-10-08 11:04:08 -0400

    Updated tempo-devtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { useEffect } from "react";
 import "../styles/globals.css";
 
 // Import the dev tools and initialize them
 import { TempoDevtools } from "tempo-devtools";
-TempoDevtools.init();
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_TEMPO) {
+      TempoDevtools.init();
+    }
+  }, []);
+
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
